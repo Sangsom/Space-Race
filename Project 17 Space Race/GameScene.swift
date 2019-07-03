392ef34a156dfd150a10ca2d1a888200c6e1a5bc
Rinalds Domanovs <rinalds.domanovs@gmail.com>
2019-07-03 13:33:12 +0300

    Added score label to scene

--- Project 17 Space Race/GameScene.swift
+++ Project 17 Space Race/GameScene.swift
@@ -35,6 +35,14 @@ class GameScene: SKScene {
         player.physicsBody = SKPhysicsBody(texture: player.texture!, size: player.size)
         player.physicsBody?.contactTestBitMask = 1
         addChild(player)
+
+        scoreLabel = SKLabelNode(fontNamed: "Chalkduster")
+        scoreLabel.position = CGPoint(x: 16, y: 16)
+        scoreLabel.horizontalAlignmentMode = .left
+        addChild(scoreLabel)
+
+        score = 0
+        
     }
 
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
