823c4f898571212b327a612474104345d10931ec
Rinalds Domanovs <rinalds.domanovs@gmail.com>
2019-07-03 13:31:24 +0300

    Added star field and player to scene

--- Project 17 Space Race/GameScene.swift
+++ Project 17 Space Race/GameScene.swift
@@ -22,7 +22,19 @@ class GameScene: SKScene {
 
 
     override func didMove(to view: SKView) {
+        backgroundColor = .black
 
+        starField = SKEmitterNode(fileNamed: "starfield")!
+        starField.position = CGPoint(x: 1024, y: 384)
+        starField.advanceSimulationTime(10)
+        addChild(starField)
+        starField.zPosition = -1
+
+        player = SKSpriteNode(imageNamed: "player")
+        player.position = CGPoint(x: 100, y: 384)
+        player.physicsBody = SKPhysicsBody(texture: player.texture!, size: player.size)
+        player.physicsBody?.contactTestBitMask = 1
+        addChild(player)
     }
 
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
